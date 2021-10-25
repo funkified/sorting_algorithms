c5ae5a22160e10f213917d0c2462df18e3ce9a56
Sergio Vera <sergio.vera@icloud.com>
2021-10-25 14:09:47 +0000

    fixing for loop

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,7 +13,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 1; j < size - 1; j++)
+		for (j = 0; j < size - 1; j++)
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
@@ -22,5 +22,4 @@ void bubble_sort(int *array, size_t size)
 				print_array(array, size);
 			}
 	}
-
 }
