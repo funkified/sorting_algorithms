7ff9d962b8c76be68f28c14865b1524fcf6f96eb
Sergio Vera <sergio.vera@icloud.com>
2021-10-25 13:54:41 +0000

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort an array using bubble sort algorithms
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < size - 1; j++)
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+	}
+
+}
---
+++ 1-insertion_sort.c
@@ -0,0 +1,9 @@
+#include "sort.h"
+/**
+ * insertion_sort - 
+ */
+
+void insertion_sort(listint_t **list)
+{
+
+}
